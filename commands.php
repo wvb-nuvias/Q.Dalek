5b4415d2b330411c3d8b85f2a3b125cd992a10a0
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2023-10-29 18:02:06 +0100

    added libraries, trying gets in ajax changed return data from php commands page

--- commands.php
+++ commands.php
@@ -1,18 +1,20 @@
 <?php
+header('Content-Type: application/json');
+
 $cmd=$_REQUEST["cmd"];
 
 switch($cmd) {
     case "reboot":
         echo "rebooting...";
         shell_exec("sudo systemctl reboot");
+        $data=["result" => "ok", "" => "rebooting..."];
         break;
     case "update":
         echo "updating...";
         shell_Exec("sudo su wouter -c /usr/local/bin/update");
-        
-        echo "  <script type=\"text/javascript\"> 
-                    window.location.href=\"index.php\"
-                </script>";
+        $data=["result" => "ok", "" => "updating..."];
         break;
 }
+
+echo json_encode($data);
 ?>
--- index.php
+++ index.php
@@ -1,20 +1,32 @@
 <html>
     <head>
+        <script src="https://kit.fontawesome.com/06b2b4d018.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.tailwindcss.com" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
         <script>
             function exterminate() {
                 document.getElementById('sound_exterminate').play();
             }
 
-            function reboot() {
-                document.location.href="commands.php?cmd=reboot";
+            function reboot() {                
+                $.getJSON('commands.php?cmd=reboot', function(data) {
+                    console.log(data); 
+                });
             }
 
             function update() {
-                document.location.href="commands.php?cmd=update";
+                $.getJSON('commands.php?cmd=update', function(data) {
+                    console.log(data); 
+                });                
             }
         </script>
     </head>
-    <body style="font-family: tahoma; user-select: none">        
+    <body style="font-family: tahoma; user-select: none">
+        <!-- TODO get sound commands from database somehow -->
+        <!-- TODO interface to add sounds? upload? -->        
+        <!-- TODO soundboard page -->
+
         <a onclick="exterminate()" href="#">Exterminate</a>        
         <br><br><br>
         <a onclick="reboot()" href="#">Reboot</a>        
