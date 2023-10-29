48264fd8e63a32cbc28b1a554d1637fdf4124dbf
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2023-10-29 16:19:28 +0100

    do not do via apache2

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
         </script>
     </head>
     <body style="font-family: tahoma">
+        <!--
         <a onclick="exterminate()" href="#">Exterminate</a>        
+        -->
+
+        <audio controls>
+            <source src="sounds/exterminate.wav" type="audio/wav">
+        </audio>
     </body>
 </html>
