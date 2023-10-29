a1b232ad3612abc664d9e1e01cea15ed2bc2fe20
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2023-10-29 14:55:34 +0100

    again

--- play.php
+++ play.php
@@ -3,7 +3,7 @@ $q=$_REQUEST["q"];
 
 switch ($q) {
     case "exterminate":
-        shell_exec(" aplay /home/wouter/exterminate.wav");        
+        shell_exec("/usr/bin/aplay /home/wouter/exterminate.wav");        
         echo "{result : 'ok'}";
         break;        
 
