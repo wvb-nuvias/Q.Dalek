603caaa58b435b7c774a63c25c0f0df2a65673a9
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2023-10-29 18:04:58 +0100

    again

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <script src="https://kit.fontawesome.com/06b2b4d018.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.tailwindcss.com" crossorigin="anonymous"></script>
+        <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
         <script>
