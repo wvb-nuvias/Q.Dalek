6ddfd1ab122a3bed2aaed1852dc539e58431358c
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2023-10-29 16:23:52 +0100

    fixing

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
     <head>
         <script>
             function exterminate() {
-                fetch('http://10.0.6.41/play.php?q=exterminate');
+                document.getElementById('sound_exterminate').play();;
             }
         </script>
     </head>
     <body style="font-family: tahoma">
-        <!--
+        
         <a onclick="exterminate()" href="#">Exterminate</a>        
-        -->
+        
 
-        <audio controls>
+        <audio id="sound_exterminate">
             <source src="sounds/exterminate.wav" type="audio/wav">
         </audio>
     </body>
