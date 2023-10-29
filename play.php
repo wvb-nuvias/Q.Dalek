4f3eacd17e77e821d2cc6c7efcbb2503e5e77dea
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2023-10-29 16:11:02 +0100

    again

--- play.php
+++ play.php
@@ -3,7 +3,7 @@ $q=$_REQUEST["q"];
 
 switch ($q) {
     case "exterminate":
-        shell_exec("playwav html/qdalek/sounds/exterminate.wav");        
+        shell_exec("/usr/local/bin/playwav /var/www/html/qdalek/sounds/exterminate.wav");        
         echo "{result : 'ok'}";
         break;        
 
