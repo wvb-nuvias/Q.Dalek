c75d538b53b6ae1e57694b4d376ad17ab79bf23d
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2023-10-29 19:22:44 +0100

    again

--- index.php
+++ index.php
@@ -34,17 +34,21 @@
         <div class="grid grid-flow-col grid-cols-3 grid-rows-3 justify-items-stretch gap-4 w-full h-full p-4">
             <button onclick="exterminate()" class="rounded-md bg-gray-400">
                 <div class="flex flex-col">
-                    <div><i class="fa-solid fa-2xl fa-skull"></i></div>
-                    <div>Exterminate</div>
+                    <div class="h-3/5"><i class="fa-solid fa-2xl fa-skull"></i></div>
+                    <div class="h-2/5">Exterminate</div>
                 </div>
             </button>
-            <button onclick="update()" class="flex flex-col rounded-md bg-green-400">
-                <div><i class="fa-solid fa-2xl fa-refresh"></i></div>
-                <div>Update</div>
+            <button onclick="update()" class="rounded-md bg-green-400">
+                <div class="flex flex-col">
+                    <div class="h-3/5"><i class="fa-solid fa-2xl fa-refresh"></i></div>
+                    <div class="h-2/5">Update</div>
+                </div>
             </button>
-            <button onclick="reboot()" class="flex flex-col rounded-md bg-red-400">
-                <div><i class="fa-solid fa-2xl fa-refresh"></i></div>
-                <div>Reboot</div>
+            <button onclick="reboot()" class="rounded-md bg-red-400">
+                <div class="flex flex-col">
+                    <div class="h-3/5"><i class="fa-solid fa-2xl fa-refresh"></i></div>
+                    <div class="h-2/5">Reboot</div>
+                </div>
             </button>
 
         </div>
