934f8614e46af92f009875539fec40353e6db422
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2023-10-29 21:00:33 +0100

    changed colors

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
         ?>
 
         <div class="grid grid-flow-col grid-cols-4 grid-rows-3 gap-<?=$margin?> w-screen h-screen p-<?=$margin?>">
-            <button onclick="dome()" class="rounded-md bg-purple-400 row-span-2">
+            <button onclick="dome()" class="rounded-md bg-purple-200 row-span-2">
                 <div class="flex flex-col gap-<?=$itemmargin?>">
                     <div><img src="images/dome_active.svg"></div>
                     <div class="<?=$textsize?>">Dome</div>
@@ -57,7 +57,7 @@
                 </div>
             </button>
 
-            <button onclick="middle()" class="rounded-md bg-blue-400 row-span-2">
+            <button onclick="middle()" class="rounded-md bg-blue-200 row-span-2">
                 <div class="flex flex-col gap-<?=$itemmargin?>">
                     <div><img src="images/middle_active.svg"></div>
                     <div class="<?=$textsize?>">Middle</div>
@@ -70,7 +70,7 @@
                 </div>
             </button>
 
-            <button onclick="skirt()" class="rounded-md bg-green-400 row-span-2">
+            <button onclick="skirt()" class="rounded-md bg-green-200 row-span-2">
                 <div class="flex flex-col gap-<?=$itemmargin?>">
                     <div><img src="images/skirt_active.svg"></div>
                     <div class="<?=$textsize?>">Skirt</div>
@@ -83,7 +83,7 @@
                 </div>
             </button>
 
-            <button onclick="fender()" class="rounded-md bg-yellow-400 row-span-2">
+            <button onclick="fender()" class="rounded-md bg-yellow-200 row-span-2">
                 <div class="flex flex-col gap-<?=$itemmargin?>">
                     <div><img src="images/bottom_active.svg"></div>
                     <div class="<?=$textsize?>">Fender</div>
