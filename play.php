c0a588da10361dd73b615e751e5e4d6502982491
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2023-10-29 14:51:10 +0100

    update play.php

--- play.php
+++ play.php
@@ -3,9 +3,11 @@ $q=$_REQUEST["q"];
 
 switch ($q) {
     case "exterminate":
-        shell_exec(" aplay /home/wouter/exterminate.wav");
+        shell_exec(" aplay /home/wouter/exterminate.wav");        
+        echo "{result : 'ok'}";
         break;        
+
 }
 
-echo "{result : 'ok'}";
+
 ?>
