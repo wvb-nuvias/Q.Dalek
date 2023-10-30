2237414c8df26f9a857c2f0225715bdbb23e29c8
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2023-10-31 00:43:06 +0100

    again

--- settings/speech.php
+++ settings/speech.php
@@ -4,7 +4,7 @@
         Speech Settings
     </div>
     <div class="grid grid-flow-col grid-cols-4 grid-rows-3 gap-<?=$margin?> h-full">
-        <div onclick="" class="rounded-md bg-purple-400 col-span-4 m-<?=$margin?>">
+        <div onclick="" class="rounded-md bg-purple-400 col-span-4 p-<?=$margin?>">
             <input id="minmax-range" type="range" min="0" max="30" value="5" class="w-full h-2 bg-purple-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
 
             <div class="flex flex-col gap-<?=$itemmargin?>">
