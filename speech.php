71dcad4fc9cf98d98415563e14eeb12f574dc282
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2023-10-30 02:47:53 +0100

    fix speech page

--- speech.php
+++ speech.php
@@ -5,17 +5,28 @@
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
-        <script>            
+        <script>    
+            function speak(text) {                
+                $.getJSON('commands.php?cmd=speak&text=' + text)
+                .done(function(data) {
+                    console.log(data.message); 
+                    console.log(data.file); 
+                    document.getElementById("output").src=data.file;
+                    document.getElementById("output").play();                   
+                });                            
+            }
             
-
             function back() {
                 document.location.href="/index.php";
             }
         </script>
     </head>
-    <body style="font-family: tahoma; user-select: none">
-        
+    <body style="font-family: tahoma; user-select: none">        
         <?php
+            //TODO get the texts from the database, add way to add and delete
+            //TODO way to do this realtime
+            //TODO set pitch etc in settings
+
             $iconsize="text-8xl";
             $textsize="text-4xl";
             $margin="2";            
@@ -23,81 +34,47 @@
         ?>
 
         <div class="grid grid-flow-col grid-cols-4 grid-rows-3 gap-<?=$margin?> w-screen h-screen p-<?=$margin?>">
-            <button onclick="decreasevolume()" class="rounded-md bg-gray-400">
+            <button onclick="speak('I am a Dahlekk')" class="rounded-md bg-green-300 col-span-3">
                 <div class="flex flex-col gap-<?=$itemmargin?>">
-                    <div class="h-4/5 <?=$iconsize?> fa-solid fa-volume-low"></div>
-                    <div class="h-1/5 <?=$textsize?>">Volume +</div>
+                    <div id="decreasevolume_icon" class="h-4/5 <?=$iconsize?> fa-solid "></div>
+                    <div class="h-1/5 <?=$textsize?>">I am a Dalek</div>
                 </div>
             </button>
-            <button onclick="togglevolume()" class="rounded-md bg-green-400">
+            <button onclick="speak('I am your soldier')" class="rounded-md bg-purple-300 col-span-3">
                 <div class="flex flex-col gap-<?=$itemmargin?>">
-                    <div class="h-4/5 <?=$iconsize?> fa-solid fa-volume-xmark"></div>
-                    <div class="h-1/5 <?=$textsize?>">Mute</div>
+                    <div id="togglevolume_icon" class="h-4/5 <?=$iconsize?> fa-solid "></div>
+                    <div class="h-1/5 <?=$textsize?>">I am your soldier</div>
                 </div>
             </button>
-            <button onclick="increasevolume()" class="rounded-md bg-red-400">
+            <button onclick="" class="rounded-md bg-pink-300 col-span-3">
                 <div class="flex flex-col gap-<?=$itemmargin?>">
-                    <div class="h-4/5 <?=$iconsize?> fa-solid fa-volume-high"></div>
-                    <div class="h-1/5 <?=$textsize?>">Volume -</div>
+                    <div id="increasevolume_icon" class="h-4/5 <?=$iconsize?> fa-solid "></div>
+                    <div class="h-1/5 <?=$textsize?>"></div>
                 </div>
             </button>
-
-            <button onclick="exterminate()" class="rounded-md bg-gray-400">
+                        
+            <button onclick="previous()" class="rounded-md bg-green-400">
                 <div class="flex flex-col gap-<?=$itemmargin?>">
-                    <div class="h-4/5 <?=$iconsize?> fa-solid fa-skull"></div>
-                    <div class="h-1/5 <?=$textsize?>">Exterminate</div>
+                    <div class="h-4/5 <?=$iconsize?> fa-solid fa-chevron-left"></div>
+                    <div class="h-1/5 <?=$textsize?>">Previous</div>
                 </div>
             </button>
-            <button onclick="update()" class="rounded-md bg-green-400">
+            <button onclick="next()" class="rounded-md bg-purple-400">
                 <div class="flex flex-col gap-<?=$itemmargin?>">
-                    <div class="h-4/5 <?=$iconsize?> fa-solid fa-refresh"></div>
-                    <div class="h-1/5 <?=$textsize?>">Update</div>
+                    <div class="h-4/5 <?=$iconsize?> fa-solid fa-chevron-right"></div>
+                    <div class="h-1/5 <?=$textsize?>">Next</div>
                 </div>
             </button>
-            <button onclick="reboot()" class="rounded-md bg-red-400">
-                <div class="flex flex-col gap-<?=$itemmargin?>">
-                    <div class="h-4/5 <?=$iconsize?> fa-solid fa-power-off"></div>
-                    <div class="h-1/5 <?=$textsize?>">Reboot</div>
-                </div>
-            </button>
-
-            <button onclick="exterminate()" class="rounded-md bg-gray-400">
-                <div class="flex flex-col gap-<?=$itemmargin?>">
-                    <div class="h-4/5 <?=$iconsize?> fa-solid fa-skull"></div>
-                    <div class="h-1/5 <?=$textsize?>">Exterminate</div>
-                </div>
-            </button>
-            <button onclick="update()" class="rounded-md bg-green-400">
-                <div class="flex flex-col gap-<?=$itemmargin?>">
-                    <div class="h-4/5 <?=$iconsize?> fa-solid fa-refresh"></div>
-                    <div class="h-1/5 <?=$textsize?>">Update</div>
-                </div>
-            </button>
-            <button onclick="reboot()" class="rounded-md bg-red-400">
-                <div class="flex flex-col gap-<?=$itemmargin?>">
-                    <div class="h-4/5 <?=$iconsize?> fa-solid fa-power-off"></div>
-                    <div class="h-1/5 <?=$textsize?>">Reboot</div>
-                </div>
-            </button>
-            
-            <button onclick="update()" class="rounded-md bg-green-400">
-                <div class="flex flex-col gap-<?=$itemmargin?>">
-                    <div class="h-4/5 <?=$iconsize?> fa-solid fa-refresh"></div>
-                    <div class="h-1/5 <?=$textsize?>">Update</div>
-                </div>
-            </button>
-            <button onclick="reboot()" class="rounded-md bg-red-400">
-                <div class="flex flex-col gap-<?=$itemmargin?>">
-                    <div class="h-4/5 <?=$iconsize?> fa-solid fa-power-off"></div>
-                    <div class="h-1/5 <?=$textsize?>">Reboot</div>
-                </div>
-            </button>
-            <button onclick="back()" class="rounded-md bg-yellow-400">
+            <button onclick="back()" class="rounded-md bg-pink-400">
                 <div class="flex flex-col gap-<?=$itemmargin?>">
                     <div class="h-4/5 <?=$iconsize?> fa-solid fa-house"></div>
                     <div class="h-1/5 <?=$textsize?>">Back</div>
                 </div>
             </button>
         </div>
+        
+        <audio id="output">
+            <source src="" type="audio/wav">
+        </audio>
     </body>
 </html>
