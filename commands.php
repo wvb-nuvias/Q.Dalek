08984900ac9c9dacda69ce608ef560d5f6931afc
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2023-10-30 02:19:04 +0100

    updates

--- commands.php
+++ commands.php
@@ -3,6 +3,18 @@ header('Content-Type: application/json');
 
 $cmd=$_REQUEST["cmd"];
 
+$path=dirname(realpath("commands.php"));
+
+$configfile = $path."/config.json";
+if (file_exists($configfile)) {
+    $json = file_get_contents($configfile); 
+    $cfg = json_decode($json,true);
+} else {
+    //write config file defaults
+}
+
+$data=["configfile" => $configfile];
+
 switch($cmd) {
     case "reboot":        
         shell_exec("sudo systemctl reboot");
@@ -22,15 +34,14 @@ switch($cmd) {
         $data=["result" => "ok", "message" => "setting volume...", "volume" => $volume];
         break;
     case "getlastchange":
-        $ret=shell_Exec("sudo su wouter -c \"git log -1 --pretty=\"format:%ci\" /home/wouter/source/repos/Q.Dalek\"");
+        $ret=shell_Exec("sudo su wouter -c \"git log -1 --pretty=\"format:%ci\" ".$path."\"");
         $data=["result" => "ok", "message" => "getting git change...", "lastchange" => $ret];
         break;
     case "getlastchangeid":
-        $ret=shell_Exec("sudo su wouter -c \"git log -1 --pretty=\"format:%H\" /home/wouter/source/repos/Q.Dalek\"");
+        $ret=shell_Exec("sudo su wouter -c \"git log -1 --pretty=\"format:%H\" ".$path."\"");
         $data=["result" => "ok", "message" => "getting git change id...", "lastid" => $ret];
         break;
-    case "getremotelastid":
-        // git ls-remote -t  --heads git@github.com:wvb-nuvias/Q.Dalek.git
+    case "getremotelastid":        
         $ret=shell_Exec("sudo su wouter -c \"git ls-remote --heads git@github.com:wvb-nuvias/Q.Dalek.git\"");
         $data=["result" => "ok", "message" => "getting git remote change id...", "lastid" => $ret];
         break;
@@ -39,10 +50,8 @@ switch($cmd) {
         $chk=str_replace(" ","_",$text);
         $chkfile="/var/www/html/qdalek/speech/".$chk.".wav";
         $retfile="speech/".$chk.".wav";
-
-        if (file_exists($chkfile)) {
-            
-        } else {
+        
+        if (!file_exists($chkfile)) {
             $cmdline="speak \"".$text."\" -ven+m6 -g6 -k30 -s150 -r35 -a250 -w ".$chkfile;       
             shell_Exec($cmdline);
         }
