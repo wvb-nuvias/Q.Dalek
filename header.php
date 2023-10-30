a36caa5964f00b87680cb760de68f5b90aa77c8a
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2023-10-30 21:23:48 +0100

    make sure header is always up to date

--- header.php
+++ header.php
@@ -4,11 +4,11 @@
 ?>
 <html>
     <head>
-        <script src="https://kit.fontawesome.com/06b2b4d018.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.tailwindcss.com"></script>
-        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.css" rel="stylesheet" />
+        <script src="https://kit.fontawesome.com/06b2b4d018.js?random=<?php echo uniqid(); ?>" crossorigin="anonymous"></script>
+        <script src="https://cdn.tailwindcss.com?random=<?php echo uniqid(); ?>"></script>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js?random=<?php echo uniqid(); ?>" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.css?random=<?php echo uniqid(); ?>" rel="stylesheet" />
         
-        <script src="/functions.js"></script>
+        <script src="/functions.js?random=<?php echo uniqid(); ?>"></script>
     </head>
     <body style="font-family: tahoma; user-select: none">
